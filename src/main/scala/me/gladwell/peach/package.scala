8c5f006bb3a06e3e062ecc3b6a3cc0d4c7200100
Ricardo Gladwell <ricardo.gladwell@gmail.com>
2013-02-10 16:35:14 +0000

    Tightened access permissions on implicit.

--- src/main/scala/me/gladwell/peach/package.scala
+++ src/main/scala/me/gladwell/peach/package.scala
@@ -4,7 +4,7 @@ import java.io.File
 
 package object peach {
 
-  implicit def augmentFile(file: File) = new FileOps(file)
+  protected[peach] implicit def augmentFile(file: File) = new FileOps(file)
 
   private[peach] implicit def fileToFolder(file: File) = new Folder(file)
 
