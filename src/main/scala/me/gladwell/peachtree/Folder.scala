0f3fb64d37a1430225b62082e42964562677a38c
Ricardo Gladwell <ricardo.gladwell@itv.com>
2013-04-10 16:30:29 +0100

    Simplified files method.

--- src/main/scala/me/gladwell/peachtree/Folder.scala
+++ src/main/scala/me/gladwell/peachtree/Folder.scala
@@ -6,14 +6,7 @@ class Folder(folder: File) {
 
   assert(folder.isDirectory())
 
-  def files(): Seq[File] = {
-    def recursiveFiles(folder: File): Seq[File] = {
-      val files = folder.listFiles
-      files ++ files.filter(_.isDirectory).flatMap(recursiveFiles(_))
-    }
-
-     recursiveFiles(folder)
-  }
+  def files(): Seq[File] = findFiles((file) => true)
 
   def findFiles(f: File => Boolean): Seq[File] = {
     def recursiveFindFiles(folder: File, f: File => Boolean): Seq[File] = {
